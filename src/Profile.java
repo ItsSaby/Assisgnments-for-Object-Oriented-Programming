e4327c0dd4f3308295a904d0628341793a77798f
Miras Kamaladin <253324@astanait.edu.kz>
2025-12-15 09:55:26 +0500

    Profile.java

--- src/Profile.java
+++ src/Profile.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+class Profile {
+    private String username;
+    private Post[] posts = new Post[10];
+    private int postCount = 0;
 
-public class Profile {
+    Profile(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    void addPost(Post p) {
+        if (postCount < posts.length) {
+            posts[postCount] = p;
+            postCount++;
+        } else {
+            System.out.println("Невозможно добавить больше постов для " + username);
+        }
+    }
+
+    void showOwnPosts() {
+        System.out.println("Профиль: " + username);
+        for (int i = 0; i < postCount; i++) {
+            posts[i].show();
+        }
+        System.out.println();
+    }
+
+    public int getPostCount() {
+        return postCount;
+    }
+
+    public Post getPost(int index) {
+        return posts[index];
+    }
 }
