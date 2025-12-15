9bf0a1408a4639dde06636e6e8f89b8cb5c32883
Miras Kamaladin <253324@astanait.edu.kz>
2025-12-15 09:56:24 +0500

    Update SocialNetwork.java

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+class SocialNetwork {
+    private String networkName;
+    private Profile[] users = new Profile[10];
+    private int userCount = 0;
 
-public class SocialNetwork {
+    SocialNetwork(String networkName) {
+        this.networkName = networkName;
+    }
+
+    void addUser(Profile p) {
+        if (userCount < users.length) {
+            users[userCount] = p;
+            userCount++;
+        } else {
+            System.out.println("Невозможно добавить больше пользователей");
+        }
+    }
+
+    public int getUserCount() {
+        return userCount;
+    }
+
+    public Profile getUser(int index) {
+        return users[index];
+    }
+
+    void showAllPosts() {
+        System.out.println("=== Все посты в " + networkName + " ===");
+        for (int i = 0; i < userCount; i++) {
+            for (int j = 0; j < users[i].getPostCount(); j++) {
+                users[i].getPost(j).show();
+            }
+        }
+        System.out.println();
+    }
+
+    void showUsers() {
+        System.out.println("Пользователи в " + networkName + ":");
+        for (int i = 0; i < userCount; i++) {
+            System.out.println("- " + users[i].getUsername());
+        }
+        System.out.println();
+    }
 }
